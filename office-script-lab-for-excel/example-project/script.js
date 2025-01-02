0253a4e55c5e5b17248cfedf4f6318208d8e6e8a
Varun Pramanik <varun.pramanik@gmail.com>
2025-01-01 23:03:30 -0600

    Adding example project files

---
+++ office-script-lab-for-excel/example-project/script.js
@@ -0,0 +1,3 @@
+// @ts-check
+/// <reference path="https://raw.githubusercontent.com/varunpramanik/typedefs/refs/heads/main/office-script-lab-for-excel/excel-script-lab.d.ts" />
+// Delete the lines above after pasting into the Script Lab editor
