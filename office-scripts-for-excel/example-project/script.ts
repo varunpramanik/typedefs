e698db9c5fafc0bbde246a16d05cce0f19cd1742
Varun Pramanik <varun.pramanik@gmail.com>
2025-02-04 17:03:09 -0600

    Updating to a much more predictable implementation for Excel Online Script types

--- office-scripts-for-excel/example-project/script.ts
+++ office-scripts-for-excel/example-project/script.ts
@@ -1,3 +1,7 @@
+// @ts-check
+/// <reference path="https://raw.githubusercontent.com/varunpramanik/typedefs/refs/heads/main/office-scripts-for-excel/excel.d.ts" />
+// Delete the lines above after pasting into the Script Lab editor
+
 function main(workbook: ExcelScript.Workbook) {
     
 }
